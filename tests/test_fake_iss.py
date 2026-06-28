32e3fd707ccae39dc12e66ee780ce27f3e53c1ef
David Bambrough <djbambrough@mac.com>
2026-06-28 20:15:23 +0100

    Added test_fake_value

--- tests/test_fake_iss.py
+++ tests/test_fake_iss.py
@@ -1,3 +1,5 @@
+from datetime import datetime
+
 from iss_tracker.adapters.fake_iss import get_screen_data
 from iss_tracker.domain.models import ScreenData
 
@@ -5,3 +7,11 @@ from iss_tracker.domain.models import ScreenData
 def test_fake_iss() -> None:
     screen_data = get_screen_data()
     assert isinstance(screen_data, ScreenData)
+
+def test_fake_value() -> None:
+    screen_data = get_screen_data()
+    assert screen_data.latitude == -43.19
+    assert screen_data.longitude == -84.5
+    assert screen_data.region == "South America"
+    assert screen_data.next_visible == datetime(2026, 6, 29, 2, 39, 0)
+    assert screen_data.timestamp == datetime(2026, 6, 28, 19, 42, 0)
